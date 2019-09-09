d941cfe04201772c0979d7d448717dd0bcc49c60
zehuatan <54189203+zehuatan@users.noreply.github.com>
2019-09-09 15:10:15 -0500

    Class notes Car

---
+++ Class Notes/Car.java
@@ -0,0 +1,67 @@
+
+/**
+ * Write a description of class Car here.
+ * This class models a car that drives and consumes fuel. It also has a license plate.
+ * 
+ * @author ztan
+ * @version 9 Sept 2019
+ */
+public class Car  // public means others can see and use it
+{
+    /*
+     * When defining a method, specify:
+     *      the visibility (e.g. public)
+     *      the return type (e.g. void)
+     *      the method name (e.g. drive)
+     *      the parameters and their types (e.g. distance of type double)
+     */
+    
+    /**  
+     * Drives the car a specified distance, in miles, while consuming fuel
+     * 
+     * @param distance the distance, in miles, the car drives
+     */
+    public void drive(double distance)
+    {
+        // write method headers first and test and fill in the method bodies later
+    }
+    /**
+     * Adds the specified amount of fuel to this car's tank
+     * 
+     * @param fuel the amount of fuel, in gallons, to add to the car's tank
+     */
+    public void addFuel(double amount)
+    {
+        
+    }
+    
+    /**
+     * Returns the amount of fuel in this car's tank
+     * 
+     * @return the amount of fuel, in gallons, in this car's tank
+     */
+    public double getFuelInTank()
+    {
+        return 0;
+    }
+    
+    /**
+     * Sets the license plate to the specified value
+     * 
+     * @param plate the license plate for this car
+     */
+    public void setLicensePlate(String plate)
+    {
+        
+    }
+    
+    /**
+     * Gets the license plate of the car
+     * 
+     * @return the license plate for this car
+     */
+    public String getLicensePlate()
+    {
+       return ""; 
+    }
+}
