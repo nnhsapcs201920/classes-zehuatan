1bb80d1e2ce94e2bc09f1ca2e3bd831f0b80384d
Zehua Tan <ZTan@student.naperville203.org>
2019-10-07 15:09:50 -0500

    cityscape windows progress

--- CityscapeLab/Building.java
+++ CityscapeLab/Building.java
@@ -32,22 +32,27 @@ public class Building
     {
         int dimensionRatio = (this.height) / (this.width) ;
         Rectangle body = new Rectangle(this.cornerX, this.cornerY, this.width, this.height);
-        Rectangle window = new Rectangle(this.cornerX + (this.width / dimensionRatio),
-        this.cornerY + (this.height / dimensionRatio), dimensionRatio * 10, dimensionRatio * 10);
+        Rectangle window = new Rectangle(this.cornerX + (this.width / (dimensionRatio * 10)),
+        this.cornerY + (this.height / (dimensionRatio * 10)), dimensionRatio * 10, dimensionRatio * 10);
         g2.setColor(Color.GRAY);
         g2.fill(body);
         g2.setColor(Color.BLACK);
         g2.draw(body);
         //fix math such that windows appear at regular intervals within the body of the building, and repeat
         //for every row.
-        for (int i = 0; i < dimensionRatio; i++)
+        for (int i = 0; i < (this.height / dimensionRatio / 20); i++)
         {
-            g2.setColor(Color.CYAN);
-            g2.fill(window);
-            g2.setColor(Color.BLACK);
-            g2.draw(window);
-            window.setLocation(this.cornerX + (i *(this.width/10)), this.cornerY + (i *(this.height / 10)));
+            //double adsofajsif = this.getY();
+            // need to keep Y value constant for the next loop, and increment for each height
+            for (int j = 0; j < (this.width / dimensionRatio / 20); j++)
+            {
+                window.setLocation(this.cornerX + (2 * j * (dimensionRatio * 10) + (this.width / (dimensionRatio * 10))),
+                this.cornerY + (this.height / (dimensionRatio + 10)));
+                g2.setColor(Color.CYAN);
+                g2.fill(window);
+                g2.setColor(Color.BLACK);
+                g2.draw(window);
+            }
         }
-        
     }
 }
